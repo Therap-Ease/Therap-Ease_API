dec9f44cd16053d8c9a8d079c881f7351d308cc2
Thijn Senden <508700@student.fontys.nl>
2024-04-06 10:49:10 +0200

    Initial commit

---
+++ TherapEase-API/TherapEase-API/PatientData.cs
@@ -0,0 +1,11 @@
+﻿namespace TherapEase_API
+{
+    public class PatientData
+    {
+            public string? Name { get; set; }      
+            public int? Age { get; set; }      
+            public DateOnly? BirtDate { get; set; }      
+            public DateOnly? DateJoined { get; set; }      
+            public string? HealthCode { get; set; }      
+    }
+}
